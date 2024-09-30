c06a15b4df1c9828e6dcbf28e40d07a8ebf42797
harper71 <princeazuka99@gmail.com>
2024-09-30 23:10:38 +0100

    commit changes

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,12 @@
+const getNeighborhoodsList = () => {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+};
+
+// export data
+export default getNeighborhoodsList;
