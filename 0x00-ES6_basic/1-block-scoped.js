96abeaaf7a28298cc5a06be33f810b60b4c52d3d
harper71 <princeazuka99@gmail.com>
2024-10-01 11:50:39 +0100

    commit changes 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
     let task = true;
-    const task2 = false;
+    let task2 = false;
   }
 
   return [task, task2];
