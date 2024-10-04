a6c0fc2492589c45315a1023f8d67a5698c79658
harper71 <princeazuka99@gmail.com>
2024-10-04 05:44:10 +0100

    tasks 1-3

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError("Name must be a string");
+    }
+
+    this._name = name;
+
+    if (typeof length !== 'number' || isNaN(length)) {
+      throw new TypeError("Length must be a number");
+    }
+
+    this._length = length;
+
+    if (!Array.isArray(students) && typeof students !== 'string') {
+      throw new TypeError("'Expected students to be an array of strings or a string'");
+    }
+    this._students = students
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  // setter and getter for  length
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    this._length = length
+  }
+  // setter and getter for students
+
+  get students() {
+    return this._students
+  }
+
+  set students(students) {
+    this._students = students
+
+  }
+}
