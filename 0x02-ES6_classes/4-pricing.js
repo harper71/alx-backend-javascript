8d18c8a3abdbddcf81ecfa411d8bc08bc8959886
harper71 <princeazuka99@gmail.com>
2024-10-05 01:01:17 +0100

    commit changes

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-underscore-dangle */
+// eslint-disable-next-line import/extensions, no-unused-vars
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // methods
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  covertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' && conversionRate !== 'number') {
+      throw new TypeError('Both amount and the convertion rate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new Error('sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
