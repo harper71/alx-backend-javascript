719426ba686ab481a7f9af8f6d60ed84205385c9
harper71 <princeazuka99@gmail.com>
2024-12-19 08:03:35 +0100

    commit changes

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,8 +4,8 @@ import { uploadPhoto } from "./5-photo-reject";
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return new Promise((resolve, reject) => {
     resolve(
-      Promise.all([signUpUser(fileName, lastName), uploadPhoto(fileName)])
-        .then()
+      Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+        .then((value))
     )
   })
 }
