03fcbd774e715f93f812e2a7f43458f2879d5264
harper71 <princeazuka99@gmail.com>
2025-01-17 19:46:35 +0100

    new data

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,18 @@
-import uploadPhoto from './5-photo-reject';
-import createUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject'; // Adjust the path based on your file structure
+import createUser from './4-user-promise'; // Adjust the path based on your file structure
 
 export default async function asyncUploadUser() {
   try {
-    const [photo, user] = await Promise.all([
-      uploadPhoto('photo-profile-1'),
-      createUser('Guillaume', 'Salva'),
-    ]);
-    return { photo, user };
-  } catch {
-    return { photo: null, user: null };
+    const photo = await uploadPhoto('photo-profile-1'); // Call uploadPhoto
+    const user = await createUser('Guillaume', 'Salva'); // Call createUser
+    return {
+      photo, // Response from uploadPhoto
+      user, // Response from createUser
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
   }
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,15 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled(promises).then((results) =>
-    results.map(({ status, value, reason }) => ({
-      status,
-      value: status === 'fulfilled' ? value : String(reason),
-    }))
-  );
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
 }
